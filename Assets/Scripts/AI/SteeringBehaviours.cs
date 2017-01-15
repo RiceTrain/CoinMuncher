e80c416314794b9261f58ae63e93642f7442b03f
RiceTrain <rhysthomas101@hotmail.com>
2017-01-15 15:06:47 +0000

    Added vehicle and SteeringBehaviours.cs classes as well as gitignore

---
+++ Assets/Scripts/AI/SteeringBehaviours.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class SteeringBehaviours {
+
+    private Vehicle _vehicle;
+
+    public void Init(Vehicle vehicleToControl)
+    {
+        _vehicle = vehicleToControl;
+    }
+
+    private Vector3 _desiredVelocity;
+	public Vector3 Seek(Vector3 targetPos)
+    {
+        _desiredVelocity = Vector3.Normalize(targetPos - _vehicle.Position) * _vehicle.MaxSpeed;
+
+        return (_desiredVelocity - _vehicle.Velocity);
+    }
+
+    public Vector3 Flee(Vector3 targetPos, float fleeRadius = 0f)
+    {
+        if((_vehicle.Position - targetPos).sqrMagnitude > fleeRadius * fleeRadius)
+        {
+            return Vector3.zero;
+        }
+
+        _desiredVelocity = Vector3.Normalize(_vehicle.Position - targetPos) * _vehicle.MaxSpeed;
+
+        return (_desiredVelocity - _vehicle.Velocity);
+    }
+
+    public enum Deceleration { slow = 3, normal = 2, fast = 1 }
+    private Vector3 _toTarget;
+    private float _distance;
+    private float _speed;
+    public Vector3 Arrive(Vector3 targetPos, Deceleration deceleration, float decelerationTweaker = 0.3f)
+    {
+        _toTarget = targetPos - _vehicle.Position;
+        _distance = _toTarget.magnitude;
+        if(_distance > 0f)
+        {
+            _speed = _distance / ((float)deceleration * decelerationTweaker);
+            _speed = Mathf.Min(_speed, _vehicle.MaxSpeed);
+
+            _desiredVelocity = _toTarget * (_speed / _distance);
+
+            return _desiredVelocity - _vehicle.Velocity;
+        }
+
+        return Vector3.zero;
+    }
+
+    private float _relativeHeading;
+    private float _lookAheadTime;
+    public Vector3 Pursuit(Vehicle evader)
+    {
+        _toTarget = evader.Position - _vehicle.Position;
+
+        _relativeHeading = Vector3.Dot(_vehicle.Heading, evader.Heading);
+        if(Vector3.Dot(_toTarget, _vehicle.Heading) > 0f && _relativeHeading < -0.95f)
+        {
+            return Seek(evader.Position);
+        }
+
+        _lookAheadTime = _toTarget.magnitude / (_vehicle.MaxSpeed / evader.Speed);
+
+        return Seek(evader.Position + (evader.Velocity * _lookAheadTime));
+    }
+}
---
+++ Assets/Scripts/AI/Vehicle.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+abstract public class Vehicle : MonoBehaviour {
+
+    private Transform VehicleTransform;
+    private Rigidbody AttachedRigidbody;
+    internal SteeringBehaviours _steeringBehaviours;
+
+    public Vector3 Position
+    {
+        get { return VehicleTransform.position; }
+    }
+
+    public Vector3 Velocity
+    {
+        get { return AttachedRigidbody.velocity; }
+        set { AttachedRigidbody.velocity = value; }
+    }
+
+    public float Speed
+    {
+        get { return AttachedRigidbody.velocity.magnitude; }
+    }
+
+    private Vector3 _heading;
+    public Vector3 Heading
+    {
+        get { return _heading; }
+    }
+
+    private Vector3 _side;
+    public Vector3 Side
+    {
+        get { return _side; }
+    }
+
+    private float _mass
+    {
+        get { return AttachedRigidbody.mass; }
+    }
+
+    private float _maxTurnRate
+    {
+        get { return AttachedRigidbody.maxAngularVelocity; }
+    }
+    
+    [SerializeField]
+    private float maxSpeed = 8f;
+    public float MaxSpeed
+    {
+        get { return maxSpeed; }
+    }
+
+    [SerializeField]
+    private float maxForce = 4f;
+    public float MaxForce
+    {
+        get { return maxForce; }
+    }
+
+    private void Awake()
+    {
+        VehicleTransform = GetComponent<Transform>();
+        AttachedRigidbody = GetComponent<Rigidbody>();
+
+        _steeringBehaviours.Init(this);
+    }
+
+    private Vector3 _steeringForce = Vector3.zero;
+    private Vector3 _acceleration = Vector3.zero;
+    private void Update()
+    {
+        _steeringForce = UpdateSteering();
+
+        _acceleration = _steeringForce / _mass;
+
+        Velocity += _acceleration * Time.deltaTime;
+        Velocity = Vector3.ClampMagnitude(Velocity, MaxSpeed);
+
+        if(Velocity.sqrMagnitude > 0.00000001f)
+        {
+            _heading = AttachedRigidbody.velocity.normalized;
+            _side = Vector3.Cross(AttachedRigidbody.velocity.normalized, VehicleTransform.up);
+        }
+    }
+
+    abstract protected Vector3 UpdateSteering();
+}
