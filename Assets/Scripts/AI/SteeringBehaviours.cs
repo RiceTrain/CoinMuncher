87f8a8c06f1ae161cb9d44212119bde66a320453
RiceTrain <rhysthomas101@hotmail.com>
2017-01-16 17:20:00 +0000

    Added evade and partial obstacle avoidance implementation

--- Assets/Scripts/AI/SteeringBehaviours.cs
+++ Assets/Scripts/AI/SteeringBehaviours.cs
@@ -2,6 +2,8 @@
 
 public class SteeringBehaviours {
 
+    //[NOTE] Need to write methods of tagging obstacles, also may need to write world to local space functions based on velocities
+
     private Vehicle _vehicle;
 
     public void Init(Vehicle vehicleToControl)
@@ -65,5 +67,63 @@ public class SteeringBehaviours {
         _lookAheadTime = _toTarget.magnitude / (_vehicle.MaxSpeed / evader.Speed);
 
         return Seek(evader.Position + (evader.Velocity * _lookAheadTime));
+    }
+
+    public Vector3 Evade(Vehicle pursuer)
+    {
+        _toTarget = pursuer.Position - _vehicle.Position;
+
+        _lookAheadTime = _toTarget.magnitude / (_vehicle.MaxSpeed + pursuer.Speed);
+
+        return Flee(pursuer.Position + (pursuer.Velocity * _lookAheadTime));
+    }
+
+    private Vector3 _wanderTarget;
+    private float _wanderJitter;
+    private float _wanderRadius;
+    private Vector3 _targetLocal;
+    private float _wanderDistance;
+    private Vector3 _targetWorld;
+    public Vector3 Wander()
+    {
+        _wanderTarget += new Vector3(Random.Range(-1, 1) * _wanderJitter, _wanderTarget.y, Random.Range(-1, 1) * _wanderJitter);
+        _wanderTarget.Normalize();
+        _wanderTarget *= _wanderRadius;
+
+        _targetLocal = _wanderTarget + new Vector3(_wanderDistance, 0f, _wanderDistance);
+        _targetWorld = _vehicle.transform.TransformPoint(_targetLocal);
+
+        return _targetWorld - _vehicle.Position;
+    }
+    
+    private float _minDetectionBoxLength = 2f;
+    private float _boxLength;
+    private Transform _closestIntersectingObstacle;
+    private float _distToClosestObstacle;
+    private Vector3 _localPosOfClosestObstacle;
+    private Vector3 _localPosition;
+    public Vector3 ObstacleAvoidance(Transform[] obstacles)
+    {
+        _boxLength = _minDetectionBoxLength + (_vehicle.Speed / _vehicle.MaxSpeed) * _minDetectionBoxLength;
+
+        //Insert method of tagging obstacles in the range of the box
+
+        _closestIntersectingObstacle = null;
+        _distToClosestObstacle = float.MaxValue;
+        _localPosOfClosestObstacle = Vector3.zero;
+
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            //Check if obstacle is tagged, only consider when it is
+
+            _localPosition = _vehicle.transform.InverseTransformPoint(obstacles[i].position);
+            _localPosition.y = _vehicle.transform.position.y;
+            if(_localPosition.z >= 0f)
+            {
+                //Check if obstacle and vehicle could be intersecting
+            }
+        }
+
+        return Vector3.zero;
     }
 }
